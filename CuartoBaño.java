18e98bbe2447c825b3bf7d53f5c167c2d45cd213
Gabriel <owelcontacto@gmail.com>
2024-02-19 09:43:03 +0100

    Primer commit

---
+++ CuartoBaño.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class CuartoBaño here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CuartoBaño
+{
+    private int metrosCuadrados;
+
+    /**
+     * Constructor for objects of class CuartoBaño
+     */
+    public CuartoBaño()
+    {
+        metrosCuadrados = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int getMetrosCuadrados()
+    {
+        return metrosCuadrados;
+    }
+}
