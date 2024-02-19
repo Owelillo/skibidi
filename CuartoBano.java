d5ddd5710d806773f75ccfbe3501eabb9d129d9a
OctavioRojasBarredo <octaviorojasbarredo@gmail.com>
2024-02-19 09:50:24 +0100

    Segundo skibidi-commit

---
+++ CuartoBano.java
@@ -0,0 +1,42 @@
+
+/**
+ * Write a description of class CuartoBaño here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CuartoBano
+{
+    private int metrosCuadrados;
+    private String formaBano;
+    private Retrete retrete;
+
+    /**
+     * Constructor for objects of class CuartoBaño
+     */
+    public CuartoBano()
+    {
+        metrosCuadrados = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int getMetrosCuadrados()
+    {
+        return metrosCuadrados;
+    }
+
+    public String getFormaBano()
+    {
+        return formaBano;
+    }
+
+    public String getRetrete()
+    {
+        return retrete;
+    }
+}
