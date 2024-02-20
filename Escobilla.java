faee837a101cef1adc973eb4453276918efed54d
Gabriel <owelcontacto@gmail.com>
2024-02-20 10:54:59 +0100

    Quinto skibidi-commit

--- Escobilla.java
+++ Escobilla.java
@@ -1,4 +1,4 @@
-package skibidi;
+ 
 
 public class Escobilla
 {
---
+++ Retrete.java
@@ -0,0 +1,23 @@
+public class Retrete
+{
+    private String marcaRetrete;
+
+    /**
+     * Constructor for objects of class Retrete
+     */
+    public Retrete()
+    {
+        String marcaRetrete = "RoCACAlcinada";
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public String getMarcaRetrete()
+    {
+        return marcaRetrete;
+    }
+}
